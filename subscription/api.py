0b775c6d18524d774f6d187024f8dcc86b5d854b
Mike Jones <mike@westerncapelabs.com>
2014-07-01 17:51:26 +0200

    Add initial tests and fix api_key creation signal

--- subscription/api.py
+++ subscription/api.py
@@ -6,9 +6,9 @@ from subscription.models import Subscription
 # ensures endpoints require username and api_key values to access
 from django.contrib.auth.models import User
 from django.db import models
-from tastypie.models import create_api_key
+# from tastypie.models import create_api_key
 
-models.signals.post_save.connect(create_api_key, sender=User)
+# models.signals.post_save.connect(create_api_key, sender=User)
 
 class SubscriptionResource(ModelResource):
     class Meta:
