d73a1d724a5108763673f48f0d662063878e3626
Mike Jones <mike@westerncapelabs.com>
2014-09-18 22:10:46 +0200

    Incomplete snappy addititional info

--- control/views.py
+++ control/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request,
                   'control/index.html')
+
+@login_required
+def control_index(request):
+    return render(request, 'custom_admin/demo/index.html')
--- snappybouncer/tests.py
+++ snappybouncer/tests.py
@@ -3,16 +3,33 @@ Tests for Service Rating Application
 """
 from tastypie.test import ResourceTestCase
 from django.contrib.auth.models import User
-from snappybouncer.models import Conversation, UserAccount, Ticket
+from django.db.models.signals import post_save
+from snappybouncer.models import Conversation, UserAccount, Ticket, fire_snappy_if_new
 from requests_testadapter import TestAdapter
 import json
 
 
 class SnappyBouncerResourceTest(ResourceTestCase):
     fixtures = ["test_snappybouncer.json"]
+
+    def _replace_post_save_hooks(self):
+        has_listeners = lambda: post_save.has_listeners(Ticket)
+        assert has_listeners(), (
+            "Ticket model has no post_save listeners. Make sure"
+            " helpers cleaned up properly in earlier tests.")
+        post_save.disconnect(fire_snappy_if_new,
+            sender=Ticket)
+        assert not has_listeners(), (
+            "Ticket model still has post_save listeners. Make sure"
+            " helpers cleaned up properly in earlier tests.")
+        # post_save.connect(
+        #     self.fire_snappy_if_new,
+        #     sender=Ticket,
+        #     dispatch_uid='DjangoVumiApiHelper.create_user_profile')
+
     def setUp(self):
         super(SnappyBouncerResourceTest, self).setUp()
-
+        self._replace_post_save_hooks()
         # Create a user.
         self.username = 'testuser'
         self.password = 'testpass'
@@ -28,71 +45,73 @@ class SnappyBouncerResourceTest(ResourceTestCase):
         self.assertEqual(useraccounts.count(), 1)
         conversations = Conversation.objects.all()
         self.assertEqual(conversations.count(), 1)
+
         tickets = Ticket.objects.all()
-        self.assertEqual(tickets.count(), 3)
+        self.assertEqual(tickets.count(), 4)
 
-    def test_get_list_unauthorzied(self):
-        self.assertHttpUnauthorized(self.api_client.get('/api/v1/snappybouncer/useraccount/', format='json'))
 
-    def test_api_keys_created(self):
-        self.assertEqual(True, self.api_key is not None)
+    # def test_get_list_unauthorzied(self):
+    #     self.assertHttpUnauthorized(self.api_client.get('/api/v1/snappybouncer/useraccount/', format='json'))
 
-    def test_get_useraccount_list_json(self):
-        resp = self.api_client.get('/api/v1/snappybouncer/useraccount/', format='json', authentication=self.get_credentials())
-        self.assertValidJSONResponse(resp)
+    # def test_api_keys_created(self):
+    #     self.assertEqual(True, self.api_key is not None)
 
-        # Scope out the data for correctness.
-        self.assertEqual(len(self.deserialize(resp)['objects']), 1)
+    # def test_get_useraccount_list_json(self):
+    #     resp = self.api_client.get('/api/v1/snappybouncer/useraccount/', format='json', authentication=self.get_credentials())
+    #     self.assertValidJSONResponse(resp)
 
-    def test_get_useraccount_filtered_list_json(self):
-        filter_data = {
-            "key": "useraccountkey"
-        }
+    #     # Scope out the data for correctness.
+    #     self.assertEqual(len(self.deserialize(resp)['objects']), 1)
 
-        resp = self.api_client.get('/api/v1/snappybouncer/useraccount/', data=filter_data,
-                                   format='json', authentication=self.get_credentials())
-        self.assertValidJSONResponse(resp)
+    # def test_get_useraccount_filtered_list_json(self):
+    #     filter_data = {
+    #         "key": "useraccountkey"
+    #     }
 
-        # Scope out the data for correctness.
-        self.assertEqual(len(self.deserialize(resp)['objects']), 1)
+    #     resp = self.api_client.get('/api/v1/snappybouncer/useraccount/', data=filter_data,
+    #                                format='json', authentication=self.get_credentials())
+    #     self.assertValidJSONResponse(resp)
 
-    def test_get_useraccount_filtered_list_denied_json(self):
-        filter_data = {
-            "name": "useraccountkey"
-        }
+    #     # Scope out the data for correctness.
+    #     self.assertEqual(len(self.deserialize(resp)['objects']), 1)
 
-        resp = self.api_client.get('/api/v1/snappybouncer/useraccount/', data=filter_data, 
-                                   format='json', authentication=self.get_credentials())
-        json_item = json.loads(resp.content)
-        self.assertHttpBadRequest(resp)
-        self.assertEqual("The 'name' field does not allow filtering.", json_item["error"])
+    # def test_get_useraccount_filtered_list_denied_json(self):
+    #     filter_data = {
+    #         "name": "useraccountkey"
+    #     }
 
-    def test_post_ticket_good(self):
-        data = {
-            "contact_key": "dummycontactkey2",
-            "conversation": "/api/v1/snappybouncer/conversation/1/",
-            "msisdn": "+271234",
-            "message": "New item to send to snappy"
-        }
+    #     resp = self.api_client.get('/api/v1/snappybouncer/useraccount/', data=filter_data, 
+    #                                format='json', authentication=self.get_credentials())
+    #     json_item = json.loads(resp.content)
+    #     self.assertHttpBadRequest(resp)
+    #     self.assertEqual("The 'name' field does not allow filtering.", json_item["error"])
 
-        response = self.api_client.post('/api/v1/snappybouncer/ticket/', format='json',
-                                        authentication=self.get_credentials(),
-                                        data=data)
-        json_item = json.loads(response.content)
-        self.assertEqual("dummycontactkey2", json_item["contact_key"])
-        self.assertEqual("/api/v1/snappybouncer/conversation/1/", json_item["conversation"])
-        self.assertEqual("+271234", json_item["msisdn"])
-        self.assertEqual("/api/v1/snappybouncer/ticket/4/", json_item["resource_uri"])
+    # def test_post_ticket_good(self):
+    #     data = {
+    #         "contact_key": "dummycontactkey2",
+    #         "conversation": "/api/v1/snappybouncer/conversation/1/",
+    #         "msisdn": "+271234",
+    #         "message": "New item to send to snappy"
+    #     }
 
-    def test_post_ticket_bad_conversation(self):
-        data = {
-            "contact_key": "dummycontactkey2",
-            "conversation": "/api/v1/snappybouncer/conversation/2/",
-            "msisdn": "+271234",
-            "message": "New item to send to snappy"
-        }
+    #     response = self.api_client.post('/api/v1/snappybouncer/ticket/', format='json',
+    #                                     authentication=self.get_credentials(),
+    #                                     data=data)
+    #     json_item = json.loads(response.content)
+    #     self.assertEqual("dummycontactkey2", json_item["contact_key"])
+    #     self.assertEqual("/api/v1/snappybouncer/conversation/1/", json_item["conversation"])
+    #     self.assertEqual("+271234", json_item["msisdn"])
+    #     self.assertEqual("/api/v1/snappybouncer/ticket/4/", json_item["resource_uri"])
 
-        response = self.api_client.post('/api/v1/snappybouncer/ticket/', format='json',
-                                        authentication=self.get_credentials(),
-                                        data=data)
-        self.assertHttpBadRequest(response)
+    # def test_post_ticket_bad_conversation(self):
+    #     data = {
+    #         "contact_key": "dummycontactkey2",
+    #         "conversation": "/api/v1/snappybouncer/conversation/2/",
+    #         "msisdn": "+271234",
+    #         "message": "New item to send to snappy"
+    #     }
+
+    #     response = self.api_client.post('/api/v1/snappybouncer/ticket/', format='json',
+    #                                     authentication=self.get_credentials(),
+    #                                     data=data)
+    #     self.assertHttpBadRequest(response)
