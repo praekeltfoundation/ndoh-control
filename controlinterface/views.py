a4af14f3cf6daec5e401e4ef2ec2189aae11604f
Mike Jones <mike@westerncapelabs.com>
2014-10-15 15:27:16 +0200

    Remove commented view code

--- controlinterface/views.py
+++ controlinterface/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-# from controlinterface.forms import AuthLoginForm
 
 @login_required(login_url='/controlinterface/login/')
 def index(request):
