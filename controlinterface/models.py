a38186cec8d99887dbc0b4b14aace7a1af96bd31
Mike Jones <mike@westerncapelabs.com>
2014-10-23 14:46:08 +0200

    Temp for new develop

--- controlinterface/models.py
+++ controlinterface/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Dashboard(models.Model):
+    pass
+
+
+    class Meta:
+        permissions = (
+            ("view_dashboard_private", "Can see all private dashboard data"),
+            ("view_dashboard_public", "Can see all public dashboard data"),
+            ("view_dashboard_summary", "Can see all summary dashboard data"),
+        )
+
+class Widget(models.Model):
+    pass
+
