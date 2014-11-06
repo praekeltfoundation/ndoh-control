0959839fd85a0aebdf5640c6282d8c4fb8ce0419
gsvr <george@westerncapelabs.com>
2014-11-06 17:41:32 +0200

    change schedule on switching message sets

--- subsend/tasks.py
+++ subsend/tasks.py
@@ -7,6 +7,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from go_http.send import HttpApiSender, LoggingSender
 import control.settings as settings
 from subscription.models import Subscription, Message
+from djcelery.models import PeriodicTask
 
 logger = get_task_logger(__name__)
 
@@ -66,6 +67,8 @@ def processes_message(subscriber, sender):
                     subscriber.next_sequence_number = 1
                     subscription = subscriber
                     subscription.message_set = message_set.next_set
+                    # Set msg schedule to standard two_per_week
+                    subscription.schedule = PeriodicTask.objects.get(pk=3)
                     subscription.save()
             else:
                 # More in this set so interate by one
