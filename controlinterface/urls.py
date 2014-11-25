966e69069d8d2eb1323e1ede73e75dc18854e3c9
gsvr <george@westerncapelabs.com>
2014-11-25 16:52:55 +0200

    add subscriptions views & urls

--- controlinterface/urls.py
+++ controlinterface/urls.py
@@ -7,7 +7,9 @@ from controlinterface.forms import AuthLoginForm
 urlpatterns = patterns('',
     # Setting the urlpatterns to hook into the api urls
     url(r'^controlinterface/$', views.index, name='index'),
+    url(r'^controlinterface/subscriptions/$', views.subscriptions, name='subscriptions'),
     # url(r'^controlinterface/login/$', views.login, name='login'),
-    url(r'^controlinterface/login/$', 'django.contrib.auth.views.login', {'template_name': 'controlinterface/login.html', 'authentication_form' : AuthLoginForm }, name='login'),
-
+    url(r'^controlinterface/login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'controlinterface/login.html',
+        'authentication_form' : AuthLoginForm }, name='login'),
 )
--- controlinterface/views.py
+++ controlinterface/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import requests
 
 @login_required(login_url='/controlinterface/login/')
 def index(request):
@@ -10,3 +11,46 @@ def index(request):
     else:
         return render(request,
                       'controlinterface/index_nodash.html')
+
+
+@login_required(login_url='/controlinterface/login/')
+def subscriptions(request):
+    if (request.user.has_perm('controlinterface.view_dashboard_private') or
+            request.user.has_perm('controlinterface.view_dashboard_summary')):
+
+        url = 'http://localhost:8000/api/v1/subscription/'
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'ApiKey george:dGVzdDp0ZXN0'
+        }
+        params = {
+            'to_addr': '+27727372369'
+        }
+
+        try:
+            subs = requests.get(url, headers=headers, params=params)
+
+            subscriptions = []
+
+            for obj in subs.json()[u'objects']:
+                subscription = {
+                    'subscriber': obj[u'to_addr'],
+                    'message_set': obj[u'message_set'][u'short_name'],
+                    'next_message': obj[u'next_sequence_number'],
+                    'language': obj[u'lang'],
+                    'active': obj[u'active'],
+                    'completed': obj[u'completed'],
+                    'schedule': obj[u'schedule'][u'name'],
+                }
+                subscriptions.append(subscription)
+
+            context = {
+                'subscriptions': subscriptions
+            }
+
+            return render(request,
+                          'controlinterface/subscriptions.html',
+                          context)
+        except:
+            return render(request,
+                          'controlinterface/index_nodash.html')
