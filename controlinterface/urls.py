511243bc9a52771549b4a9ea554f2b03cd4b042f
Mike Jones <mike@westerncapelabs.com>
2014-11-26 18:08:07 +0200

    Add metrics API urls

--- controlinterface/urls.py
+++ controlinterface/urls.py
@@ -1,11 +1,17 @@
-from django.conf.urls import patterns, url
-from controlinterface import views
+from django.conf.urls import patterns, url, include
+from controlinterface import views, api
 from controlinterface.forms import AuthLoginForm
+from tastypie.api import Api
 
-
+# Setting the API base name and registering the API resources using
+# Tastypies API function
+api_resources = Api(api_name='v1/controlinterface')
+api_resources.register(api.MetricResource())
+api_resources.prepend_urls()
 
 urlpatterns = patterns('',
     # Setting the urlpatterns to hook into the api urls
+    url(r'^api/', include(api_resources.urls)),
     url(r'^controlinterface/$', views.index, name='index'),
     # url(r'^controlinterface/login/$', views.login, name='login'),
     url(r'^controlinterface/login/$', 'django.contrib.auth.views.login', {'template_name': 'controlinterface/login.html', 'authentication_form' : AuthLoginForm }, name='login'),
