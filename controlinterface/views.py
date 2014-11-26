10ec7adddbcd4236e1b1190d0813803583deba40
Mike Jones <mike@westerncapelabs.com>
2014-11-26 18:08:28 +0200

    update views for metric via local

--- controlinterface/views.py
+++ controlinterface/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+
 @login_required(login_url='/controlinterface/login/')
 def index(request):
     if (request.user.has_perm('controlinterface.view_dashboard_private') or
@@ -10,3 +11,4 @@ def index(request):
     else:
         return render(request,
                       'controlinterface/index_nodash.html')
+
