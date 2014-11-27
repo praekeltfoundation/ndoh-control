f5e617e1abf16627718473738fbce14e85d8a620
Mike Jones <mike@westerncapelabs.com>
2014-11-27 22:27:15 +0200

    Add Service Rating to CI

--- controlinterface/urls.py
+++ controlinterface/urls.py
@@ -13,6 +13,8 @@ urlpatterns = patterns('',
     # Setting the urlpatterns to hook into the api urls
     url(r'^api/', include(api_resources.urls)),
     url(r'^controlinterface/$', views.index, name='index'),
+    url(r'^controlinterface/servicerating/download/', views.servicerating_report, name='servicerating_report'),
+    url(r'^controlinterface/servicerating/', views.servicerating, name='servicerating'),
     url(r'^controlinterface/login/$', 'django.contrib.auth.views.login', {'template_name': 'controlinterface/login.html', 'authentication_form' : AuthLoginForm }, name='login'),
 
 )
