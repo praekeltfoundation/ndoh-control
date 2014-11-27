24e8ab1413b26d9bb845439dd05df3812805db16
Mike Jones <mike@westerncapelabs.com>
2014-11-27 11:13:39 +0200

    Remove commented URL

--- controlinterface/urls.py
+++ controlinterface/urls.py
@@ -13,7 +13,6 @@ urlpatterns = patterns('',
     # Setting the urlpatterns to hook into the api urls
     url(r'^api/', include(api_resources.urls)),
     url(r'^controlinterface/$', views.index, name='index'),
-    # url(r'^controlinterface/login/$', views.login, name='login'),
     url(r'^controlinterface/login/$', 'django.contrib.auth.views.login', {'template_name': 'controlinterface/login.html', 'authentication_form' : AuthLoginForm }, name='login'),
 
 )
