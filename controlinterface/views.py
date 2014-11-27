1f8c9eb6c9350ce05f8bd223f33c1f7b7c9449e6
gsvr <george@westerncapelabs.com>
2014-11-27 16:54:44 +0200

    start process as text

--- controlinterface/views.py
+++ controlinterface/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from models import Dashboard
 
 
 @login_required(login_url='/controlinterface/login/')
 def index(request):
     if (request.user.has_perm('controlinterface.view_dashboard_private') or
             request.user.has_perm('controlinterface.view_dashboard_summary')):
+
+        dashboard = Dashboard.objects.get(pk=1)
+
+        widgets = dashboard.widgets.all()
+
+        widget_data = {}
+
+        for widget in widgets:
+            widget_data[widget.id] = widget.data.all()
+
+        context = {
+            "widgets": dashboard.widgets.all(),
+            "widget_data": widget_data
+        }
+
         return render(request,
-                      'controlinterface/index.html')
+                      'controlinterface/index.html',
+                      context)
     else:
         return render(request,
                       'controlinterface/index_nodash.html')
