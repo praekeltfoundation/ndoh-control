74387057b6394854b8da9a6c2655c774f1e8b1cf
gsvr <george@westerncapelabs.com>
2015-08-28 14:19:18 +0200

    provide for dry tests

--- registration/tests.py
+++ registration/tests.py
@@ -7,6 +7,23 @@ from rest_framework import status
 from .models import Source, Registration
 
 
+TEST_REG_DATA = {
+    "hcw_msisdn": None,
+    "mom_msisdn": "+27001",
+    "mom_id_type": "sa_id",
+    "mom_lang": "en",
+    "mom_edd": "2015-08-01",
+    "mom_id_no": "8009151234001",
+    "mom_dob": None,
+    "clinic_code": "12345",
+    "authority": "clinic"
+}
+
+TEST_SOURCE_DATA = {
+    "name": "Test Source"
+}
+
+
 class APITestCase(TestCase):
 
     def setUp(self):
@@ -29,17 +46,24 @@ class AuthenticatedAPITestCase(APITestCase):
         self.adminclient.credentials(
             HTTP_AUTHORIZATION='Token ' + self.admintoken)
 
-
-class TestRegistrationsAPI(AuthenticatedAPITestCase):
-
-    def make_source(self, name="Test Source"):
-        post_data = {
-            "name": name,
-        }
+    def make_source(self, post_data=TEST_SOURCE_DATA):
         response = self.adminclient.post('/api/v1/sources/',
                                          json.dumps(post_data),
                                          content_type='application/json')
         return response
+
+    def make_registration(self, post_data=TEST_REG_DATA):
+        source = self.make_source()
+        source_id = source.data["id"]
+        post_data["source"] = "/api/v1/sources/%s/" % source_id
+
+        response = self.adminclient.post('/api/v1/registrations/',
+                                         json.dumps(post_data),
+                                         content_type='application/json')
+        return response
+
+
+class TestRegistrationsAPI(AuthenticatedAPITestCase):
 
     def test_create_source(self):
         response = self.make_source()
@@ -51,26 +75,8 @@ class TestRegistrationsAPI(AuthenticatedAPITestCase):
         self.assertEqual(d.id, 2)
 
     def test_create_registration(self):
-        source = self.make_source()
-        source_id = source.data["id"]
-
-        post_data = {
-            "hcw_msisdn": None,
-            "mom_msisdn": "+27001",
-            "mom_id_type": "sa_id",
-            "mom_lang": "en",
-            "mom_edd": "2015-08-01",
-            "mom_id_no": "8009151234001",
-            "mom_dob": None,
-            "clinic_code": "12345",
-            "authority": "clinic",
-            "source": "/api/v1/sources/%s/" % source_id
-        }
-        response = self.adminclient.post('/api/v1/registrations/',
-                                         json.dumps(post_data),
-                                         content_type='application/json')
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        reg_response = self.make_registration()
+        self.assertEqual(reg_response.status_code, status.HTTP_201_CREATED)
 
         d = Registration.objects.last()
         self.assertEqual(d.id, 1)
